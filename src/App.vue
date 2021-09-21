08b0bf449d266bb64ac05e251f86fa9811e5a6ce
AndyBeable <andy.beable@gmail.com>
2021-09-21 15:26:48 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header></the-header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/layout/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Spartan:wght@200;400;500&display=swap');
+
+html {
+  font-family: 'Spartan', sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #effafa;
 }
 </style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div :style="image" class="image"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: { backgroundImage: "url('/src/assets/bg-header-desktop.svg')" },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.image {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 8rem;
+  width: 100%;
+  background-color: #5ba4a4;
+}
+</style>
