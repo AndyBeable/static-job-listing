4a4f9f794e08dc2d40125f75379f9cae42b0de89
AndyBeable <andy.beable@gmail.com>
2021-09-22 17:19:41 +0100

    data json

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
@@ -6,6 +7,11 @@ import BaseCard from './components/ui/BaseCard.vue';
 import JobList from './components/layout/JobList.vue';
 
 const app = createApp(App);
+const store = createStore({
+  state() {
+    return {};
+  },
+});
 
 app.component('base-card', BaseCard);
 app.component('job-list', JobList);
